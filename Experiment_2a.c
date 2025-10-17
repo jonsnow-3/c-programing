7b25c5dc50200c8c29c5da3efbe448a334d3fd7c
jonsnow-3 <saifbhati917@gmail.com>
2025-10-17 16:01:17 +0530

    Update Experiment_2a.c

--- Experiment_2a.c
+++ Experiment_2a.c
@@ -1,14 +1,21 @@
+/*Name of Student : Saif Arif Bhati
+UIN 251P077
+Class : F.E. Computer Engineering 
+Div : D 
+Age : 19*/
 #include <stdio.h>
 
 int main() {
-    int num;
-
-    // Input from user
-    printf("Enter a number: ");
-    scanf("%d", &num);
-
-    // Check even or odd using conditional operator
-    (num % 2 == 0) ? printf("The number %d is Even.\n", num) : printf("The number %d is Odd.\n", num);
-
-    return 0;
+    
+int num;
+printf("ENTER THE NUMBER: ");
+scanf("%d", &num);
+if(num % 2 == 0){ 
+printf(" THE NUM IS EVEN");
+}
+else{
+printf("THE NUM IS ODD");
+    
+}
+return 0;
 }
