60e3eaa97d0acc05adacf2887ab6d13ada8234f4
jonsnow-3 <saifbhati917@gmail.com>
2025-10-17 14:21:18 +0530

    Create Experiment_1a.c

---
+++ Experiment_1a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Name: Saif Arif Bhati\n");
+    printf("Aget: 19\n");
+    printf("Class: FE Computer Engineering\n");
+    printf("Division: D\n");
+    printf("UIN: 251P082\n");
+    return 0;
+}
