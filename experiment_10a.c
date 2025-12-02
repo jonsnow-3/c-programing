77b3c95dbe6d4a084392ddb5509b547d7bc1c15a
jonsnow-3 <saifbhati917@gmail.com>
2025-12-02 11:05:26 +0530

    Update experiment_10a.c

--- experiment_10a.c
+++ experiment_10a.c
@@ -3,3 +3,21 @@ UIN 251P077
 Class : F.E. Computer Engineering 
 Div : D 
 Age : 19*/
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    int *p, *q;
+
+    p = &a;
+    q = &b;
+
+    printf("Enter first number: ");
+    scanf("%d", p);
+    printf("Enter second number: ");
+    scanf("%d", q);
+
+    sum = *p + *q;
+    printf("Sum = %d", sum);
+    return 0;
+}
