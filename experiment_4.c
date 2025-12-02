1e2afb5c539f2f34c1bd95ed521e9e3da9a850bf
jonsnow-3 <saifbhati917@gmail.com>
2025-12-02 10:09:07 +0530

    Update experiment_4.c

--- experiment_4.c
+++ experiment_4.c
@@ -3,3 +3,33 @@ UIN 251P077
 Class : F.E. Computer Engineering 
 Div : D 
 Age : 19*/
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+    for (int i = 2; i <= num / 2; i++) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+void printPrimes(int lower, int upper) {
+    printf("Prime numbers between %d and %d are:\n", lower, upper);
+    for (int i = lower; i <= upper; i++) {
+        if (isPrime(i))
+            printf("%d ", i);
+    }
+    printf("\n");
+}
+
+int main() {
+    int start, end;
+    printf("Enter lower limit: ");
+    scanf("%d", &start);
+    printf("Enter upper limit: ");
+    scanf("%d", &end);
+    printPrimes(start, end);
+    return 0;
+}
