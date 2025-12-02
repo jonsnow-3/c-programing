07973dd2ce52ced1dde736041f490b7f57bbb407
jonsnow-3 <saifbhati917@gmail.com>
2025-12-02 11:06:44 +0530

    Update experiment_10b.c

--- experiment_10b.c
+++ experiment_10b.c
@@ -3,3 +3,27 @@ UIN 251P077
 Class : F.E. Computer Engineering 
 Div : D 
 Age : 19*/
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i;
+    int *p;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+
+    p = &a[n - 1];
+
+    printf("Array in reverse order:\n");
+    for (i = n - 1; i >= 0; i--) {
+        printf("%d ", *p);
+        p--;
+    }
+
+    return 0;
+}
