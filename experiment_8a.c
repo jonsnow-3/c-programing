c47355c7a075387c06316718e9ec8e76305f2989
jonsnow-3 <saifbhati917@gmail.com>
2025-12-02 10:15:32 +0530

    Update experiment_8a.c

--- experiment_8a.c
+++ experiment_8a.c
@@ -3,3 +3,25 @@ UIN 251P077
 Class : F.E. Computer Engineering 
 Div : D 
 Age : 19*/
+#include <stdio.h>
+
+int findLength(char str[]) {
+    int i, count = 0;
+    for (i = 0; str[i] != '\0'; i++) {
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    char str[100];
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+    int len = findLength(str);
+    if (str[len - 1] == '\n') {
+        str[len - 1] = '\0';
+        len--;
+    }
+    printf("Length of string = %d", len);
+    return 0;
+}
