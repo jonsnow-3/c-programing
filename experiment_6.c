29881740f5ae31a55a19596b11aec953742bcb39
jonsnow-3 <saifbhati917@gmail.com>
2025-12-02 10:11:31 +0530

    Update experiment_6.c

--- experiment_6.c
+++ experiment_6.c
@@ -3,3 +3,25 @@ UIN 251P077
 Class : F.E. Computer Engineering 
 Div : D 
 Age : 19*/
+#include <stdio.h>
+
+void counter() {
+    static int count = 0;
+    count++;
+    printf("The counter function has been called %d times.\n", count);
+}
+
+int main() {
+    int n, i;
+    printf("Enter how many times you want to call the counter function: ");
+    scanf("%d", &n);
+    
+    printf("\nCalling the counter function %d times:\n\n", n);
+    
+    for (i = 1; i <= n; i++) {
+        counter();
+    }
+
+    printf("\nProgram finished. The counter function was executed %d times in total.\n", n);
+    return 0;
+}
