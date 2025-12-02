43af4f9210c2a0fb83b03da5be5b550ea9c061f3
jonsnow-3 <saifbhati917@gmail.com>
2025-12-02 10:10:21 +0530

    Update experiment_5.c

--- experiment_5.c
+++ experiment_5.c
@@ -3,3 +3,28 @@ UIN 251P077
 Class : F.E. Computer Engineering 
 Div : D 
 Age : 19*/
+#include <stdio.h>
+
+long long factorialIterative(int n) {
+    long long fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    return fact;
+}
+
+long long factorialRecursive(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorialRecursive(n - 1);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Factorial of %d using iterative method = %lld\n", num, factorialIterative(num));
+    printf("Factorial of %d using recursive method = %lld\n", num, factorialRecursive(num));
+    return 0;
+}
